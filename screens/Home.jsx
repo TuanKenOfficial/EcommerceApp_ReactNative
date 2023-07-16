d9562df9845f72dca9f24016d64ba8cf515981e5
TuanKenOfficial <nhatyeuthu2013@gmail.com>
2023-07-16 15:21:12 +0700

    Du an ưng dung thuong mai dien tu đang phat trien them chuc nang

---
+++ screens/Home.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet,View, Text } from "react-native";
+import React from "react";
+
+
+const Home = () =>{
+    return(
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home
+
+const style = StyleSheet.create({});
---
+++ screens/Profile.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet,View, Text } from "react-native";
+import React from "react";
+
+
+const Profile = () =>{
+    return(
+        <View>
+            <Text>Profile</Text>
+        </View>
+    )
+}
+
+export default Profile
+
+const style = StyleSheet.create({});
